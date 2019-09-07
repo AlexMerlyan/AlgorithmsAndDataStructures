f7a8f14f868a0a4f670b031d2532eeaad587b73e
AlexMerlyan <sasha-merlyan@yandex.com>
2019-09-07 09:46:46 +0300

    Added reverse integer implementation

--- src/main/java/algorithms/tasks/ReverseInteger.java
+++ src/main/java/algorithms/tasks/ReverseInteger.java
@@ -4,9 +4,9 @@ public class ReverseInteger {
 
     public static void main(String[] args) {
         ReverseInteger reverseInteger = new ReverseInteger();
-        System.out.println(reverseInteger.reverseInt(123456, 6));
-        System.out.println(reverseInteger.reverseInt(54321, 5));
-        System.out.println(reverseInteger.reverseInt(123, 3));
+        System.out.println(reverseInteger.reverseInt(123456));
+        System.out.println(reverseInteger.reverseInt(54321));
+        System.out.println(reverseInteger.reverseInt(123));
     }
 
     private int reverseInt(int number, int length, int divider, int startPow) {
@@ -18,12 +18,24 @@ public class ReverseInteger {
         return result + reverseInt(number, length, divider * 10, startPow);
     }
 
-    public int reverseInt(int number, int length) {
-
+    public int reverseInt(int number) {
+        int length = getLength(number);
         int pow = (int) Math.pow(10, --length);
         int divider = 10;
         int result = number % divider * pow;
         return result + reverseInt(number, length, divider * 10, pow);
+    }
+
+    private int getLength(int number) {
+        int length = 0;
+        for (int i = 10; i > 0; i--) {
+            int pow = (int) Math.pow(10, i -1);
+            if (number / pow > 0) {
+                length = i;
+                break;
+            }
+        }
+        return length;
     }
 
 }
